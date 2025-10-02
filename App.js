907092777ea4d612786f96c190be946573f65988
bruni3ra <igor.bruniera@escola.pr.gov.br>
2025-10-02 13:07:17 +0000

    e456

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Meunome from './componentes/primeiroComponente'
+import { SafeAreaView } from 'react-native-web';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bruniera</Text>
-      <Meunome/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView>
+      </StatusBar>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </SafeAreaView>
+  )
+}
---
+++ componente/telas/Topo.js
@@ -0,0 +1,18 @@
+import react from "react";
+import { Image,StyleSheet,Dimensions } from "react-native-web";
+
+import Texto from "./Texto";
+import { StyleSheet } from "react-native/types_generated/index";
+
+export default function Topo(titulo){
+    return <>
+    <Texto style={estilos.titulo}>  {titulo}   </Texto>
+    </>
+}
+
+cons estilos = StyleSheet.create({
+    topo;{
+        width: "100%",
+        height: 578 / 786 * width,
+    }
+});
